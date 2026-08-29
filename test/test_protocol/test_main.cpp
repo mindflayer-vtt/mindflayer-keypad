354fce6e0af6f004abfbcccdbe8ae7ea8894cf76
Jan Giesenberg <jan@giesenberg.de>
2026-08-29 14:54:47 +0200

    test: cover keypad configuration parsing

--- test/test_protocol/test_main.cpp
+++ test/test_protocol/test_main.cpp
@@ -60,11 +60,46 @@ void test_requires_q_shift_and_space_for_restart() {
   TEST_ASSERT_FALSE(mindflayer::protocol::shouldRestart(true, true, false));
 }
 
+void test_parses_canonical_led_configuration() {
+  DynamicJsonDocument document(1024);
+  mindflayer::protocol::Configuration configuration;
+  TEST_ASSERT_TRUE(mindflayer::protocol::parseConfiguration(
+    document,
+    "{\"type\":\"configuration\",\"controller-id\":\"controller1\","
+      "\"led1\":{\"r\":1,\"g\":2,\"b\":3},"
+      "\"led2\":{\"r\":4,\"g\":5,\"b\":6}}",
+    configuration
+  ));
+  TEST_ASSERT_EQUAL_UINT8(1, configuration.led1.r);
+  TEST_ASSERT_EQUAL_UINT8(2, configuration.led1.g);
+  TEST_ASSERT_EQUAL_UINT8(3, configuration.led1.b);
+  TEST_ASSERT_EQUAL_UINT8(4, configuration.led2.r);
+  TEST_ASSERT_EQUAL_UINT8(5, configuration.led2.g);
+  TEST_ASSERT_EQUAL_UINT8(6, configuration.led2.b);
+}
+
+void test_rejects_malformed_and_unrelated_configuration_messages() {
+  DynamicJsonDocument document(1024);
+  mindflayer::protocol::Configuration configuration;
+  TEST_ASSERT_FALSE(mindflayer::protocol::parseConfiguration(
+    document,
+    "{malformed",
+    configuration
+  ));
+  TEST_ASSERT_FALSE(mindflayer::protocol::parseConfiguration(
+    document,
+    "{\"type\":\"key-event\"}",
+    configuration
+  ));
+}
+
 int main(int, char**) {
   UNITY_BEGIN();
   RUN_TEST(test_builds_canonical_controller_registration);
   RUN_TEST(test_builds_canonical_key_down_and_up_events);
   RUN_TEST(test_reports_truncated_protocol_messages);
   RUN_TEST(test_requires_q_shift_and_space_for_restart);
+  RUN_TEST(test_parses_canonical_led_configuration);
+  RUN_TEST(test_rejects_malformed_and_unrelated_configuration_messages);
   return UNITY_END();
 }
